39c2bf45a07a15a2277fa33ba1abe94d6e891e55
Parlance <parlance@vicr123.com>
2022-11-09 09:06:53 +0000

    Update Translations - vi_VN, ro_RO
    Co-Authored-By: Victor Tran <vicr12345@gmail.com>
    Co-Authored-By: dlphin <dlphesigns@outlook.com>

---
+++ application/translations/ro_RO.ts
@@ -0,0 +1,240 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<TS version="2.1">
+  <context>
+    <name>MainWindow</name>
+    <message>
+      <translation>theBranch</translation>
+      <location filename="../mainwindow.ui" line="14" />
+      <location filename="../../../build-theBranch-Chroot_6-Debug/application/thebranch_autogen/include/ui_mainwindow.h" line="310" />
+      <source>theBranch</source>
+    </message>
+    <message>
+      <translation>Fișier</translation>
+      <location filename="../mainwindow.ui" line="116" />
+      <location filename="../../../build-theBranch-Chroot_6-Debug/application/thebranch_autogen/include/ui_mainwindow.h" line="373" />
+      <source>File</source>
+    </message>
+    <message>
+      <translation>Depozit</translation>
+      <location filename="../mainwindow.ui" line="128" />
+      <location filename="../../../build-theBranch-Chroot_6-Debug/application/thebranch_autogen/include/ui_mainwindow.h" line="374" />
+      <source>Repository</source>
+    </message>
+    <message>
+      <translation>Remote-uri</translation>
+      <location filename="../mainwindow.ui" line="139" />
+      <location filename="../../../build-theBranch-Chroot_6-Debug/application/thebranch_autogen/include/ui_mainwindow.h" line="375" />
+      <source>Remotes</source>
+    </message>
+    <message>
+      <translation>Arbore</translation>
+      <location filename="../mainwindow.ui" line="146" />
+      <location filename="../../../build-theBranch-Chroot_6-Debug/application/thebranch_autogen/include/ui_mainwindow.h" line="376" />
+      <source>Tree</source>
+    </message>
+    <message>
+      <translation>Ieșire</translation>
+      <location filename="../mainwindow.ui" line="166" />
+      <location filename="../../../build-theBranch-Chroot_6-Debug/application/thebranch_autogen/include/ui_mainwindow.h" line="311" />
+      <source>Exit</source>
+    </message>
+    <message>
+      <translation>Control+Q</translation>
+      <location filename="../mainwindow.ui" line="169" />
+      <location filename="../../../build-theBranch-Chroot_6-Debug/application/thebranch_autogen/include/ui_mainwindow.h" line="313" />
+      <source>Ctrl+Q</source>
+    </message>
+    <message>
+      <translation>Clonează un depozit</translation>
+      <location filename="../mainwindow.ui" line="174" />
+      <location filename="../../../build-theBranch-Chroot_6-Debug/application/thebranch_autogen/include/ui_mainwindow.h" line="315" />
+      <source>Clone Repository</source>
+    </message>
+    <message>
+      <translation>F7</translation>
+      <location filename="../mainwindow.ui" line="177" />
+      <location filename="../../../build-theBranch-Chroot_6-Debug/application/thebranch_autogen/include/ui_mainwindow.h" line="317" />
+      <source>F7</source>
+    </message>
+    <message>
+      <translation>Deschide un depozit</translation>
+      <location filename="../mainwindow.ui" line="186" />
+      <location filename="../../../build-theBranch-Chroot_6-Debug/application/thebranch_autogen/include/ui_mainwindow.h" line="319" />
+      <source>Open Repository</source>
+    </message>
+    <message>
+      <translation>Control+O</translation>
+      <location filename="../mainwindow.ui" line="189" />
+      <location filename="../../../build-theBranch-Chroot_6-Debug/application/thebranch_autogen/include/ui_mainwindow.h" line="321" />
+      <source>Ctrl+O</source>
+    </message>
+    <message>
+      <translation>Push</translation>
+      <location filename="../mainwindow.ui" line="198" />
+      <location filename="../../../build-theBranch-Chroot_6-Debug/application/thebranch_autogen/include/ui_mainwindow.h" line="323" />
+      <source>Push</source>
+    </message>
+    <message>
+      <translation>Control+Up</translation>
+      <location filename="../mainwindow.ui" line="201" />
+      <location filename="../../../build-theBranch-Chroot_6-Debug/application/thebranch_autogen/include/ui_mainwindow.h" line="325" />
+      <source>Ctrl+Up</source>
+    </message>
+    <message>
+      <translation>Pull</translation>
+      <location filename="../mainwindow.ui" line="210" />
+      <location filename="../../../build-theBranch-Chroot_6-Debug/application/thebranch_autogen/include/ui_mainwindow.h" line="327" />
+      <source>Pull</source>
+    </message>
+    <message>
+      <translation>Control+Down</translation>
+      <location filename="../mainwindow.ui" line="213" />
+      <location filename="../../../build-theBranch-Chroot_6-Debug/application/thebranch_autogen/include/ui_mainwindow.h" line="329" />
+      <source>Ctrl+Down</source>
+    </message>
+    <message>
+      <translation>Checkout...</translation>
+      <location filename="../mainwindow.ui" line="222" />
+      <location filename="../../../build-theBranch-Chroot_6-Debug/application/thebranch_autogen/include/ui_mainwindow.h" line="331" />
+      <source>Checkout...</source>
+    </message>
+    <message>
+      <translation>Control+.</translation>
+      <location filename="../mainwindow.ui" line="225" />
+      <location filename="../../../build-theBranch-Chroot_6-Debug/application/thebranch_autogen/include/ui_mainwindow.h" line="333" />
+      <source>Ctrl+.</source>
+    </message>
+    <message>
+      <translation>Branșă nouă...</translation>
+      <location filename="../mainwindow.ui" line="234" />
+      <location filename="../../../build-theBranch-Chroot_6-Debug/application/thebranch_autogen/include/ui_mainwindow.h" line="335" />
+      <source>New Branch...</source>
+    </message>
+    <message>
+      <translation>Control+B</translation>
+      <location filename="../mainwindow.ui" line="237" />
+      <location filename="../../../build-theBranch-Chroot_6-Debug/application/thebranch_autogen/include/ui_mainwindow.h" line="337" />
+      <source>Ctrl+B</source>
+    </message>
+    <message>
+      <translation>Etichetă nouă...</translation>
+      <location filename="../mainwindow.ui" line="246" />
+      <location filename="../../../build-theBranch-Chroot_6-Debug/application/thebranch_autogen/include/ui_mainwindow.h" line="339" />
+      <source>New Tag...</source>
+    </message>
+    <message>
+      <translation>Control+G</translation>
+      <location filename="../mainwindow.ui" line="249" />
+      <location filename="../../../build-theBranch-Chroot_6-Debug/application/thebranch_autogen/include/ui_mainwindow.h" line="341" />
+      <source>Ctrl+G</source>
+    </message>
+    <message>
+      <translation>Îmbinare...</translation>
+      <location filename="../mainwindow.ui" line="258" />
+      <location filename="../../../build-theBranch-Chroot_6-Debug/application/thebranch_autogen/include/ui_mainwindow.h" line="343" />
+      <source>Merge...</source>
+    </message>
+    <message>
+      <translation>Control+M</translation>
+      <location filename="../mainwindow.ui" line="261" />
+      <location filename="../../../build-theBranch-Chroot_6-Debug/application/thebranch_autogen/include/ui_mainwindow.h" line="345" />
+      <source>Ctrl+M</source>
+    </message>
+    <message>
+      <translation>Rebase...</translation>
+      <location filename="../mainwindow.ui" line="270" />
+      <location filename="../../../build-theBranch-Chroot_6-Debug/application/thebranch_autogen/include/ui_mainwindow.h" line="347" />
+      <source>Rebase...</source>
+    </message>
+    <message>
+      <translation>Control+E</translation>
+      <location filename="../mainwindow.ui" line="273" />
+      <location filename="../../../build-theBranch-Chroot_6-Debug/application/thebranch_autogen/include/ui_mainwindow.h" line="349" />
+      <source>Ctrl+E</source>
+    </message>
+    <message>
+      <translation>Cherry-Pick...</translation>
+      <location filename="../mainwindow.ui" line="282" />
+      <location filename="../../../build-theBranch-Chroot_6-Debug/application/thebranch_autogen/include/ui_mainwindow.h" line="351" />
+      <source>Cherry Pick...</source>
+    </message>
+    <message>
+      <translation>Control+K</translation>
+      <location filename="../mainwindow.ui" line="285" />
+      <location filename="../../../build-theBranch-Chroot_6-Debug/application/thebranch_autogen/include/ui_mainwindow.h" line="353" />
+      <source>Ctrl+K</source>
+    </message>
+    <message>
+      <translation>Comite...</translation>
+      <location filename="../mainwindow.ui" line="294" />
+      <location filename="../../../build-theBranch-Chroot_6-Debug/application/thebranch_autogen/include/ui_mainwindow.h" line="355" />
+      <source>Commit...</source>
+    </message>
+    <message>
+      <translation>Control+Return</translation>
+      <location filename="../mainwindow.ui" line="297" />
+      <location filename="../../../build-theBranch-Chroot_6-Debug/application/thebranch_autogen/include/ui_mainwindow.h" line="357" />
+      <source>Ctrl+Return</source>
+    </message>
+    <message>
+      <translation>Stash</translation>
+      <location filename="../mainwindow.ui" line="306" />
+      <location filename="../../../build-theBranch-Chroot_6-Debug/application/thebranch_autogen/include/ui_mainwindow.h" line="359" />
+      <source>Stash</source>
+    </message>
+    <message>
+      <translation>Control+S</translation>
+      <location filename="../mainwindow.ui" line="309" />
+      <location filename="../../../build-theBranch-Chroot_6-Debug/application/thebranch_autogen/include/ui_mainwindow.h" line="361" />
+      <source>Ctrl+S</source>
+    </message>
+    <message>
+      <translation>Imprimare</translation>
+      <location filename="../mainwindow.ui" line="318" />
+      <location filename="../../../build-theBranch-Chroot_6-Debug/application/thebranch_autogen/include/ui_mainwindow.h" line="363" />
+      <source>Print</source>
+    </message>
+    <message>
+      <translation>Control+P</translation>
+      <location filename="../mainwindow.ui" line="321" />
+      <location filename="../../../build-theBranch-Chroot_6-Debug/application/thebranch_autogen/include/ui_mainwindow.h" line="365" />
+      <source>Ctrl+P</source>
+    </message>
+    <message>
+      <translation>Setări</translation>
+      <location filename="../mainwindow.ui" line="326" />
+      <location filename="../../../build-theBranch-Chroot_6-Debug/application/thebranch_autogen/include/ui_mainwindow.h" line="367" />
+      <source>Settings</source>
+    </message>
+    <message>
+      <translation>Control+,</translation>
+      <location filename="../mainwindow.ui" line="329" />
+      <location filename="../../../build-theBranch-Chroot_6-Debug/application/thebranch_autogen/include/ui_mainwindow.h" line="369" />
+      <source>Ctrl+,</source>
+    </message>
+    <message>
+      <translation>Renunță la toate modificările</translation>
+      <location filename="../mainwindow.ui" line="337" />
+      <location filename="../../../build-theBranch-Chroot_6-Debug/application/thebranch_autogen/include/ui_mainwindow.h" line="371" />
+      <source>Discard All Changes</source>
+    </message>
+    <message>
+      <translation>Comite</translation>
+      <location filename="../mainwindow.cpp" line="137" />
+      <source>Commit</source>
+    </message>
+    <message>
+      <translation>Git</translation>
+      <location filename="../mainwindow.cpp" line="240" />
+      <source>Git</source>
+    </message>
+  </context>
+  <context>
+    <name>main</name>
+    <message>
+      <translation>Client Git</translation>
+      <location filename="../main.cpp" line="40" />
+      <source>Git Client</source>
+    </message>
+  </context>
+</TS>
